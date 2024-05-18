de827e11aa29e37cec6b56abd7f7fd05e25ca4b6
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-18 11:44:31 +0500

    creating adn login for admin

--- back/src/modules/auth/auth.controller.ts
+++ back/src/modules/auth/auth.controller.ts
@@ -1,17 +1,39 @@
 import {FastifyInstance, FastifyReply, FastifyRequest} from 'fastify'
 import Admins from "../../models/admins.model";
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
-async function AdminsController(fastify: FastifyInstance, opts: any): Promise<void> {
+async function AdminsController(auth: FastifyInstance, opts: any): Promise<void> {
     
-    fastify.post('/authentication', async (request: FastifyRequest, reply: FastifyReply) => {
+    auth.post('/authentication', async (request: FastifyRequest, reply: FastifyReply) => {
         const body: any = request.body;
-        const admin = await Admins.findOne({login: body.login});
+        const admin: any = await Admins.findOne({login: body.login});
         
+        if (!admin?._id) {
+            return reply.status(400).send({
+                error: 'Bad request error',
+                message: 'Invalid login'
+            });
+        }
         
-        reply.send({status: 'ok'})
+        const isCorrectPassword = bcrypt.compareSync(body.password, admin?.password);
+
+        if (!isCorrectPassword) {
+            return reply.status(400).send({
+                error: 'Bad request error',
+                message: 'Invalid password'
+            });
+        }
+        
+        const secret: any = process.env.JWT_SECRET;
+        
+        const token = jwt.sign({id: admin._id}, secret);
+        
+        const {password: removedKey, ...user} = admin._doc;
+        
+        reply.send({accessToken: token, user: user});
     });
     
 }
-
 
 export default AdminsController;
--- back/src/mongoose.ts
+++ back/src/mongoose.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 async function connect() {
     const MONGO_URI: any = process.env.MONGO_URI;
-
+    
     mongoose.connect(MONGO_URI).catch(err => {
         console.log(err);
         process.exit(1);
-    }).then((r) => console.log('Monogdb was connected'));
+    })
 }
 
 export default connect;
