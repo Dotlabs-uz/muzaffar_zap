73e382a12b412ee29258b42f0ba74250cb945b76
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-21 01:33:24 +0500

    some changes

--- back/src/services/cars/cars.hooks.ts
+++ back/src/services/cars/cars.hooks.ts
@@ -1,4 +1,7 @@
 import * as authentication from '@feathersjs/authentication';
+import {HookContext} from '@feathersjs/feathers';
+import carsModel from "../../models/cars.model";
+import app from "../../app";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const {authenticate} = authentication.hooks;
@@ -16,7 +19,16 @@ export default {
 
     after: {
         all: [],
-        find: [],
+        find: [async (context: HookContext) => {
+            const query: any = context.params.query;
+
+            const regex = new RegExp(query.autoNumber, 'gi');
+            context.result.data = await carsModel(app).find({
+                autoNumber: {
+                    $regex: regex
+                }
+            });
+        }],
         get: [],
         create: [],
         update: [],
