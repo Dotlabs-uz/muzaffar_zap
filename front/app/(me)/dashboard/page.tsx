b704fa0533b097b0a19c66fc3b34a7ec1432b19a
jony <sabinalatipova20@gmail.com>
2024-05-20 23:28:39 +0500

    create car

--- front/app/(me)/dashboard/page.tsx
+++ front/app/(me)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { useForm } from 'react-hook-form';
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -17,133 +16,142 @@ import {
     ResizablePanelGroup,
 } from "@/components/ui/resizable"
 import Modal from '@/components/Modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+import Form from '@/components/Form';
 
 const Page = () => {
-    const { register, handleSubmit, formState: { errors }, } = useForm();
-    const [openModal, setOpenModal] = useState(false);
+    // const { register, handleSubmit, formState: { errors }, } = useForm();
+    // const [openModal, setOpenModal] = useState(false);
+    const token = cookies().get("userToken")?.value
 
-    const onSubmit = async (data: any) => {
-        // axios
+    // const onSubmit = async (data: any) => {
+    //     // axios
 
-        console.log(data);
-    };
+    //     console.log(data);
+    // };
+
+
 
     return (
-        <div className="px-3 h-screen pt-3 pb-5 w-full flex flex-col bg-black">
-            {
-                openModal && (
-                    <Modal setOpenModal={setOpenModal} />
-                )
-            }
-            <form className='flex flex-col h-full' onSubmit={handleSubmit(onSubmit)}>
-                <div className="w-full flex items-center justify-center gap-5 pt-9 pb-5 rounded-lg bg-[#121212]">
-                    <Input
-                        type="text"
-                        autoComplete='off'
-                        className={`max-w-3xl py-5 bg-[#242424] text-white ${errors.numberCar && "border border-[red] outline-[red]"}`}
-                        {...register("numberCar", { pattern: /\d{2}\s?(?:[A-Za-z]\s\d{3}|\d{3})\s?[A-Za-z]{2,3}/g, required: true })}
-                    />
-                    <Button onClick={() => setOpenModal(true)} type='button' className="bg-green-700 hover:bg-green-600 text-2xl h-11">+</Button>
-                </div>
-                <ResizablePanelGroup direction="vertical" className="mt-3 text-white">
-                    <ResizablePanel className='scroll h-fit p-4 mb-4 rounded-lg bg-[#121212]'>
-                        <Table>
-                            <TableHeader>
-                                <TableRow className='border-b hover:bg-transparent select-none border-white/20'>
-                                    <TableHead className="w-[100px] text-center">numbers</TableHead>
-                                    <TableHead className="w-[100px]">Invoice</TableHead>
-                                    <TableHead>Status</TableHead>
-                                    <TableHead>Method</TableHead>
-                                    <TableHead className="text-right">Amount</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody className='radius'>
-                                {
-                                    [0, 1, 2, 3, 4, 5, 6, 7].map((i: number) => (
-                                        <TableRow key={i} className='border-none cursor-pointer'>
-                                            <TableCell className="font-medium text-center rounded-l-lg">{i + 1}</TableCell>
-                                            <TableCell className="font-medium">INV001</TableCell>
-                                            <TableCell>Paid</TableCell>
-                                            <TableCell>Credit Card</TableCell>
-                                            <TableCell className="text-right rounded-r-lg">$250.00</TableCell>
-                                        </TableRow>
-                                    ))
-                                }
-                            </TableBody>
-                        </Table>
-                    </ResizablePanel>
-                    <ResizableHandle withHandle />
-                    <ResizablePanel className='h-full pb-8 px-5 mt-4 rounded-lg bg-[#121212]'>
-                        <div className="h-full grid grid-cols-3 gap-5 mt-5">
-                            <ul className="grid grid-cols-2 gap-2">
-                                {
-                                    [0, 1, 2, 3, 4, 5, 6, 7].map((i: number) => (
-                                        <li key={i}>
-                                            <label className={`radio-btn cursor-pointer ${errors.column && "animate-pulse"}`}>
-                                                <input type="radio" {...register("column", { required: true })} name="column" value={i + 1} className="hidden-radio" />
-                                                <span>{i + 1}</span>
-                                            </label>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                            <div className="grid grid-cols-1 gap-2">
-                                <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
-                                    <input value={"Такси"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
-                                    <span>Такси</span>
-                                </label>
-                                <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
-                                    <input value={"Грузовые"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
-                                    <span>Грузовые</span>
-                                </label>
-                                <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
-                                    <input value={"Продать"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
-                                    <span>Продать</span>
-                                </label>
-                            </div>
-                            <div className="flex flex-col">
-                                <div className="flex flex-col gap-2 h-[55%]">
-                                    <Input
-                                        type="number"
-                                        {...register("kub", { required: true })}
-                                        className={`w-full h-full text-2xl px-5 bg-[#242424] text-white ${errors.kub && "border-[red] outline-[red]"}`}
-                                        placeholder="Kub"
-                                    />
-
-                                    <Input
-                                        className="w-full h-full text-2xl px-5 bg-[#242424] text-white"
-                                        type="number"
-                                        defaultValue={"10 000"}
-                                        placeholder="Sum"
-                                    />
-
-                                    <Input
-                                        className="w-full h-full text-2xl px-5 bg-[#242424] text-white"
-                                        type="number"
-                                        placeholder="Sum"
-                                    />
+        <>
+            <Form token={token} />
+            {/* <div className="px-3 h-screen pt-3 pb-5 w-full flex flex-col bg-black">
+                {
+                    openModal && (
+                        <Modal setOpenModal={setOpenModal} />
+                    )
+                }
+                <form className='flex flex-col h-full' onSubmit={handleSubmit(onSubmit)}>
+                    <div className="w-full flex items-center justify-center gap-5 pt-9 pb-5 rounded-lg bg-[#121212]">
+                        <Input
+                            type="text"
+                            autoComplete='off'
+                            className={`max-w-3xl py-5 bg-[#242424] text-white ${errors.numberCar && "border border-[red] outline-[red]"}`}
+                            {...register("numberCar", { pattern: /\d{2}\s?(?:[A-Za-z]\s\d{3}|\d{3})\s?[A-Za-z]{2,3}/g, required: true })}
+                        />
+                        <Button onClick={() => setOpenModal(true)} type='button' className="bg-green-700 hover:bg-green-600 text-2xl h-11">+</Button>
+                    </div>
+                    <ResizablePanelGroup direction="vertical" className="mt-3 text-white">
+                        <ResizablePanel className='scroll h-fit p-4 mb-4 rounded-lg bg-[#121212]'>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow className='border-b hover:bg-transparent select-none border-white/20'>
+                                        <TableHead className="w-[100px] text-center">numbers</TableHead>
+                                        <TableHead className="w-[100px]">Invoice</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Method</TableHead>
+                                        <TableHead className="text-right">Amount</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody className='radius'>
+                                    {
+                                        [0, 1, 2, 3, 4, 5, 6, 7].map((i: number) => (
+                                            <TableRow key={i} className='border-none cursor-pointer'>
+                                                <TableCell className="font-medium text-center rounded-l-lg">{i + 1}</TableCell>
+                                                <TableCell className="font-medium">INV001</TableCell>
+                                                <TableCell>Paid</TableCell>
+                                                <TableCell>Credit Card</TableCell>
+                                                <TableCell className="text-right rounded-r-lg">$250.00</TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </ResizablePanel>
+                        <ResizableHandle withHandle />
+                        <ResizablePanel className='h-full pb-8 px-5 mt-4 rounded-lg bg-[#121212]'>
+                            <div className="h-full grid grid-cols-3 gap-5 mt-5">
+                                <ul className="grid grid-cols-2 gap-2">
+                                    {
+                                        [0, 1, 2, 3, 4, 5, 6, 7].map((i: number) => (
+                                            <li key={i}>
+                                                <label className={`radio-btn cursor-pointer ${errors.column && "animate-pulse"}`}>
+                                                    <input type="radio" {...register("column", { required: true })} name="column" value={i + 1} className="hidden-radio" />
+                                                    <span>{i + 1}</span>
+                                                </label>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                                <div className="grid grid-cols-1 gap-2">
+                                    <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
+                                        <input value={"Такси"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
+                                        <span>Такси</span>
+                                    </label>
+                                    <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
+                                        <input value={"Грузовые"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
+                                        <span>Грузовые</span>
+                                    </label>
+                                    <label className={`radio-btn cursor-pointer ${errors.type && "animate-pulse"}`}>
+                                        <input value={"Продать"} type="radio" {...register("type", { required: true })} name="type" className={`hidden-radio ${errors.type && "animate-pulse"}`} />
+                                        <span>Продать</span>
+                                    </label>
                                 </div>
-                                <div className="h-[45%] w-full">
-                                    <div className="flex items-center justify-between mt-5">
-                                        <p className="text-nowrap text-lg font-medium">Цена:</p>
-                                        <hr className="w-[55%] border-dashed border-1 border-white" />
-                                        <p className="text-nowrap">100 000 Sum</p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p className="text-nowrap text-lg font-medium">Количество:</p>
-                                        <hr className="w-[55%] border-dashed border-1 border-white" />
-                                        <p>10 шт</p>
-                                    </div>
+                                <div className="flex flex-col">
+                                    <div className="flex flex-col gap-2 h-[55%]">
+                                        <Input
+                                            type="number"
+                                            {...register("kub", { required: true })}
+                                            className={`w-full h-full text-2xl px-5 bg-[#242424] text-white ${errors.kub && "border-[red] outline-[red]"}`}
+                                            placeholder="Kub"
+                                        />
 
-                                    <Button type='submit' className="bg-green-700 hover:bg-green-600 text-lg h-11 mt-5">Submit</Button>
+                                        <Input
+                                            className="w-full h-full text-2xl px-5 bg-[#242424] text-white"
+                                            type="number"
+                                            defaultValue={"10 000"}
+                                            placeholder="Sum"
+                                        />
+
+                                        <Input
+                                            className="w-full h-full text-2xl px-5 bg-[#242424] text-white"
+                                            type="number"
+                                            placeholder="Sum"
+                                        />
+                                    </div>
+                                    <div className="h-[45%] w-full">
+                                        <div className="flex items-center justify-between mt-5">
+                                            <p className="text-nowrap text-lg font-medium">Цена:</p>
+                                            <hr className="w-[55%] border-dashed border-1 border-white" />
+                                            <p className="text-nowrap">100 000 Sum</p>
+                                        </div>
+                                        <div className="flex items-center justify-between">
+                                            <p className="text-nowrap text-lg font-medium">Количество:</p>
+                                            <hr className="w-[55%] border-dashed border-1 border-white" />
+                                            <p>10 шт</p>
+                                        </div>
+
+                                        <Button type='submit' className="bg-green-700 hover:bg-green-600 text-lg h-11 mt-5">Submit</Button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </ResizablePanel>
-                </ResizablePanelGroup>
-            </form >
-        </div >
+                        </ResizablePanel>
+                    </ResizablePanelGroup>
+                </form >
+            </div > */}
+        </>
     );
 }
 
