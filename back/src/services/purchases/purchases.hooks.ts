06e55dc48abc66c61e554c093ddad19bc5d92fda
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-23 01:48:32 +0500

    some changes

--- back/src/services/purchases/purchases.hooks.ts
+++ back/src/services/purchases/purchases.hooks.ts
@@ -2,7 +2,7 @@ import {HookContext} from '@feathersjs/feathers';
 import {iff} from 'feathers-hooks-common';
 import countBonus from '../../hooks/countBonus';
 import * as authentication from '@feathersjs/authentication';
-import discountBonus from "../../hooks/discountBonus";
+import discountBonus from '../../hooks/discountBonus';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const {authenticate} = authentication.hooks;
