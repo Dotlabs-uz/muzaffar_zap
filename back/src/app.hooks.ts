ca3401a3103132a3a176e1f513c4a4bfc43b970f
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-23 13:18:38 +0500

    some changes

--- back/src/app.hooks.ts
+++ back/src/app.hooks.ts
@@ -6,6 +6,7 @@ import {NotAuthenticated} from '@feathersjs/errors';
 import {parseJwt} from './utils/parseJWT';
 import reportsModel from './models/reports.model';
 import app from './app';
+import {iff} from "feathers-hooks-common";
 
 export default {
     before: {
@@ -22,35 +23,37 @@ export default {
         all: [],
         find: [],
         get: [],
-        create: [async (context: HookContext) => {
-            const auth: any = context.params.authentication;
+        create: [
+            iff(context => !context.path.includes('authentication'), async (context: HookContext) => {
+                const auth: any = context.params.authentication;
 
-            if (!auth?.accessToken) {
-                throw new NotAuthenticated('Not authorized');
-            }
-
-            const payload: any = parseJwt(auth.accessToken);
-
-            let operator: any = {};
-
-            try {
-                operator = await context.app.service('operators').get(payload.sub);
-            } catch (e) {
-                return;
-            }
-
-            const methods = ['create', 'patch', 'remove'];
-
-            await reportsModel(app).create({
-                method: methods.indexOf(context.method),
-                data: context.data,
-                path: context.path,
-                operator: {
-                    fullName: operator.fullName,
-                    login: operator.login
+                if (!auth?.accessToken) {
+                    throw new NotAuthenticated('Not authorized');
                 }
-            });
-        }],
+
+                const payload: any = parseJwt(auth.accessToken);
+
+                let operator: any = {};
+
+                try {
+                    operator = await context.app.service('operators').get(payload.sub);
+                } catch (e) {
+                    return;
+                }
+
+                const methods = ['create', 'patch', 'remove'];
+
+                await reportsModel(app).create({
+                    method: methods.indexOf(context.method),
+                    data: context.data,
+                    path: context.path,
+                    operator: {
+                        fullName: operator.fullName,
+                        login: operator.login
+                    }
+                });
+            })
+        ],
         update: [],
         patch: [],
         remove: []
