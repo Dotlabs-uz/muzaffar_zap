ee98fdf8d412e7a0949679fd5f57f55566b2ddc8
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-23 11:11:15 +0500

    creating reports

---
+++ back/src/hooks/createReport.ts
@@ -0,0 +1,48 @@
+import {HookContext} from '@feathersjs/feathers';
+import {NotAuthenticated} from '@feathersjs/errors';
+import {parseJwt} from '../utils/parseJWT';
+
+export default async function () {
+    return async (context: HookContext) => {
+        const data = context.data;
+
+        const cars = await context.app.service('cars').find({query: {autoNumber: data.autoNumber}});
+
+        const history = {
+            volume: data.volume,
+            price: data.price,
+            column: data.column,
+            bonusPrice: cars.data[0].bonus + data.history.bonusPrice,
+            allVolume: data.history.allVolume,
+            bonusPercent: data.history.bonusPercent
+        };
+
+        await context.app.service('cars').patch(null, {
+            $push: {
+                history: history
+            }
+        }, {query: {autoNumber: data.autoNumber}});
+
+
+        const auth: any = context.params.authentication;
+
+        if (!auth?.accessToken) {
+            throw new NotAuthenticated('Not authorized');
+        }
+
+        const payload: any = parseJwt(auth.accessToken);
+
+        let operator: any = {};
+
+        try {
+            operator = await context.app.service('operators').get(payload.sub);
+        } catch (e) {
+            return;
+        }
+
+        data.operator = {
+            fullName: operator.fullName,
+            login: operator.login
+        };
+    };
+}
--- back/src/services/purchases/purchases.hooks.ts
+++ back/src/services/purchases/purchases.hooks.ts
@@ -3,8 +3,7 @@ import {iff} from 'feathers-hooks-common';
 import countBonus from '../../hooks/countBonus';
 import * as authentication from '@feathersjs/authentication';
 import discountBonus from '../../hooks/discountBonus';
-import {NotAuthenticated} from "@feathersjs/errors";
-import {parseJwt} from "../../utils/parseJWT";
+import createReport from '../../hooks/createReport';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const {authenticate} = authentication.hooks;
@@ -17,48 +16,7 @@ export default {
         create: [
             iff((context: HookContext) => context.data.isTaxi && !context.data?.useBonus, countBonus()),
             iff((context: HookContext) => context.data?.useBonus, discountBonus()),
-            async (context: HookContext) => {
-                const data = context.data;
-
-                const cars = await context.app.service('cars').find({query: {autoNumber: data.autoNumber}});
-
-                const history = {
-                    volume: data.volume,
-                    price: data.price,
-                    column: data.column,
-                    bonusPrice: cars.data[0].bonus + data.history.bonusPrice,
-                    allVolume: data.history.allVolume,
-                    bonusPercent: data.history.bonusPercent
-                };
-
-                await context.app.service('cars').patch(null, {
-                    $push: {
-                        history: history
-                    }
-                }, {query: {autoNumber: data.autoNumber}});
-
-
-                const auth: any = context.params.authentication;
-
-                if (!auth?.accessToken) {
-                    throw new NotAuthenticated('Not authorized');
-                }
-
-                const payload: any = parseJwt(auth.accessToken);
-
-                let operator: any = {};
-
-                try {
-                    operator = await context.app.service('operators').get(payload.sub);
-                } catch (e) {
-                    return;
-                }
-
-                data.operator = {
-                    fullName: operator.fullName,
-                    login: operator.login
-                };
-            }
+            createReport(),
         ],
         update: [],
         patch: [],
